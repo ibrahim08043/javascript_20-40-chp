0e0cd59d789a3ca13ee2116bf978335aa469643b
Ibrahim Kashif <111134217+ibrahim08043@users.noreply.github.com>
2022-09-16 09:51:32 -0700

    Add files via upload

---
+++ Javascript 20 - 40 chp/app.js
@@ -0,0 +1,261 @@
+// var a = prompt("Enter the input")
+// var b = a.toUpperCase();
+// alert(b);
+
+// var a = prompt("Enter the input")
+// var b = a.toLowerCase();
+// alert(b)
+
+// var a = prompt("Enter the input");
+// var b = prompt("Enter the input");
+// var c = prompt("Enter the input");
+// var d = prompt("Enter the input");
+// alert(a+b+c+d);
+
+
+// var a = prompt("Enter the name");
+// var b = a.slice(- 1);
+// alert(b);
+
+
+// var text = prompt("Enter the name")
+//  for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) == "n") {
+//     alert("found the n");
+//     break;
+//      }
+//      }
+
+
+// var text = prompt("Enter the name")
+//  for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!",";","@","#") {
+//     alert("Exclamation point found!");
+//     break;
+//      }
+//      }
+
+
+
+// var a = ("Islamabad");
+// var b = a.replace("Islam", "Hyder");
+// alert(b);
+
+// var a = ("and");
+// var b = a.replace("and","&");
+// alert(b);
+
+
+
+// var num = Math.ceil(Math.random() * 10);
+// alert(num);
+//  var gnum = prompt('Guess the number between 1 and 10 inclusive');
+//  if (gnum == num)
+//    alert('Matched');
+//   else
+//  alert('Not matched, the number was '+gnum);
+
+
+// var b = Math.round(3.45214);
+// alert(b);
+
+// var a = Math.floor(3.45214);
+// alert(a);
+
+// var a = Math.ceil(3.45214);
+// alert(a)
+
+
+// var b = Math.round(-2.678);
+// alert(b);
+
+// var a = Math.floor(-2.678);
+// alert(a);
+
+
+// var a = Math.ceil(-2.678);
+// alert(a)
+
+
+// var a = Math.random() * 2;
+// alert(a)
+
+
+// var a = Math.ceil(Math.random() * 2);
+// if(a == 1){
+//     alert("Tails")
+// }
+// else if(a == 2){
+//     alert("Heads")
+// }
+
+
+
+// var b = prompt("Enter the secret number");
+// var a = Math.ceil(Math.random() * 10);
+// if(b == a){
+//     alert("Match");
+// }
+// else{
+//     alert("sorry sir")
+// };
+
+
+// var a = "455"
+// var b = typeof(a)
+// alert(b)
+
+
+// var a = 455
+// var b = typeof(a)
+// alert(b)
+
+
+// var a = (30 / 45) * 100;
+// var b = a.toFixed(2);
+// alert(b);
+
+// chapter # (21-30) is complete
+
+
+
+// var a = new Date;
+// alert(a)
+
+// var a = new Date;
+// var b = ("Oct")
+//  for (var i = 0; i <= 3; i++) {
+//      if (a === b[i]) {
+//      alert("It's very funny day");
+//      }
+//      }
+// alert(b)
+
+
+
+// var a = new Date();
+// var b = ("Sat")
+//  for (var i = 0; i <= 3; i++) {
+//      if (a === b[i]) {
+//      alert("It's very funny day");
+//      }
+//      }
+// alert(b)
+
+
+// var a = new Date("09-10-2022");
+// var b = ("Sat","Sun")
+//  for (var i = 0; i = 3; i++){
+//      if (a == b[i]) {
+//      alert("It's very funny day");
+//      }
+//      }
+
+
+
+// var a = new Date() ;
+// var b = a.getTime() ;
+// var c = a.getMilliseconds(01-01-1970) ;
+// alert(c);
+
+
+// var a = new Date(03-04-2022);
+// var b = a.getMinutes();
+// var c = new Date();
+// var d = c.getMinutes();
+// var f = (b-d);
+// alert(f)
+
+
+
+// var a = new Date();
+// var b = 14
+// for(i = 0; i = 2; i++) {
+//     if(a == b){
+//         alert("“First fifteen days of the month”" )
+//     }
+// }
+// else{
+//     alert("last day of the month")
+// }
+
+
+// var a = prompt("Enter the time With AM/PM");
+// var b = a.slice(-2);
+
+// if(b == "am"){
+//     alert("Its AM sir ")
+// }
+// else if (b == "pm"){
+//     alert("Its PM sir")
+// }
+// alert(b)
+
+
+
+// var a = prompt("Enter num");
+// var b = prompt("Enter num");
+// var c = (a*b);
+// alert(c);
+
+// var a = new Date() ;
+// var b = new Date(09-14-1920)
+// var c = (a + b)
+// alert(c)
+
+// var b = 2022;
+// var a = prompt("Enter the age");
+// var c = (a-b);
+// alert(c);
+
+// var a = prompt("Enter the Costomer Name");
+// var b = prompt("Enter the Months Name");
+// var c = prompt("Enter the Unit");
+// var d = prompt("Enter the charge per unit");
+// var f = + prompt("Enter the After Due Date Charge");
+// var DueDateAmount = (c*d);
+// var v = (DueDateAmount + f)
+
+// alert(`${a} ${b} ${c} ${d} ${f} ${DueDateAmount} ${v}`)
+
+// chapter # (31-34) is complete
+
+//  function tellTime(now) {
+//      var now = new Date();
+//     alert(now);
+//      }
+//     tellTime()
+
+// function abc(amount,discount){
+
+//     alert(amount + discount)
+// }
+// abc(mustafa,ali)
+
+
+
+// function abc(mus){
+
+//     var nav = (mus * 2)
+//     alert(nav)
+// }
+// abc(15)
+
+
+
+// function abc(ms,ds){
+
+//     var nav = (ms + ds)
+//     alert(nav)
+// }
+// abc(15,50)
+
+
+
+// function abc(mus,fa){
+
+//     var nav = (mus + 1)
+
+//     alert(nav)
+// }
+// abc(15,20)
